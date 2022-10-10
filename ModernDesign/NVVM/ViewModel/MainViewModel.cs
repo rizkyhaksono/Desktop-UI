b33387e28cba50c2af2c7a374954f743a23cb786
rizkyhaksono <mrizkyhaksono@gmail.com>
2022-10-11 00:32:08 +0700

    add view model to app.xaml

---
+++ ModernDesign/NVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModernDesign.Core;
+
+namespace ModernDesign.NVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
